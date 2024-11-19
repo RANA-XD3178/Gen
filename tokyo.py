d4d93e4212d69698451e8b9997277131a429ba84
RANA-XD3178 <saimadr411@gmail.com>
2024-11-19 22:20:32 +0500

    Add files via upload

---
+++ tokyo.py
@@ -0,0 +1,10 @@
+import os,platform
+os.system('git pull --quiet 2>/dev/null')
+os.system("clear")
+print('\033[97;1m [•] Join Whatsapp Group')
+os.system('xdg-open https://chat.whatsapp.com/BmxmNHAMjLj5i59vqB2uKw')
+djs=platform.architecture()[0]
+if djs=="32bit":
+    __import__("tokyo32")
+elif djs=="64bit":
+    __import__("tokyo64")
